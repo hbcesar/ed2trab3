1fbb881e1e4990cf6d3095d68049e333856dde33
Cesar Henrique Bernabe <cesar-hb@hotmail.com>
2015-05-25 18:54:57 -0300

    adicionando gera

---
+++ gera.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define MAX 100
+
+void crescente(int n){
+	int i;
+
+	for(i=1; i<=n; i++){
+		printf("%d\n", i);
+	}
+}
+
+void descrescente(int n){
+	int i;
+
+	for(i=n; i>0; i--){
+		printf("%d\n", i);
+	}
+}
+
+void aleatorio(int n){
+	int i;
+
+	srand(time(NULL));
+
+	for(i=0; i<n; i++){
+		printf("%d\n", rand() % MAX);
+	}
+}
+
+int main(int argc, char* argv []){
+	int n, tipo;
+
+	//obtem o numero de elementos a serem gerados
+	n = atoi(argv[2]);
+
+	
+	if(strcmp(argv[1], "-a")==0)
+		aleatorio(n);
+	else if(strcmp(argv[1], "-c")==0){
+		crescente(n);
+	}
+	else if (strcmp(argv[1], "-d")==0){
+		descrescente(n);
+	}
+
+	return 0;
+}
